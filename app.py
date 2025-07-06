dfbb330615e6ed972737bcaae213583d8ff26a9b
Jeredpoi <death_in_red19@mail.ru>
2025-07-06 19:12:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -135,3 +135,8 @@ def callback():
             vk.messages.send(peer_id=peer_id, message="❌ Неизвестная команда или недостаточно аргументов", random_id=0)
 
     return "ok"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+

13abc99e8e8fad12d943057ffb3e89d09cfd6059
Jeredpoi <death_in_red19@mail.ru>
2025-07-06 19:02:44 +0300

    Добавлены таблицы

--- init_db.py
+++ init_db.py
@@ -1,1 +1,23 @@
+import sqlite3
+
+conn = sqlite3.connect("users.db")
+cur = conn.cursor()
+
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        user_id TEXT PRIMARY KEY,
+        nickname TEXT
+    )
+""")
+
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS roles (
+        user_id TEXT PRIMARY KEY,
+        role TEXT
+    )
+""")
+
+conn.commit()
+conn.close()
+print("✅ Таблицы созданы.")
 

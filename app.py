94816f1c3bb355b246094d11629dacbae05067ce
Jeredpoi <death_in_red19@mail.ru>
2025-07-07 09:24:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -177,8 +177,10 @@ def callback():
             vk.messages.send(peer_id=peer_id, message="❌ Неизвестная команда или недостаточно прав", random_id=0)
 
     if __name__ == "__main__":
-        port = int(os.environ.get("PORT", 10000))
-        app.run(host="0.0.0.0", port=port)
+    import os
+    port = int(os.environ.get("PORT", 10000))  # Render передаёт порт через переменную PORT
+    app.run(host="0.0.0.0", port=port)
+
 
 
     return "ok"
